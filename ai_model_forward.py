b98ec921ee94a24f37c01c4c860e0fbe6bd4c4ee
levdik <eugene.levdik@gmail.com>
2025-06-16 09:05:31 +0200

    add gradient descent with nn

--- ai_model_forward.py
+++ ai_model_forward.py
@@ -13,10 +13,15 @@ class SquarePixelLensScatteringModel(nnx.Module):
             n_lens_params: int,
             hidden_layer_dims: list[int],
             rngs: nnx.Rngs,
+            include_transmission=False
     ):
+        self.include_transmission = include_transmission
         self.n_lens_params = n_lens_params
         self.n_propagating_waves = n_propagating_waves
-        output_dim = 4 * n_propagating_waves
+        if include_transmission:
+            output_dim = 4 * n_propagating_waves
+        else:
+            output_dim = 2 * n_propagating_waves
         layer_dims = [n_lens_params] + hidden_layer_dims + [output_dim]
         layers = []
 
@@ -36,12 +41,13 @@ class SquarePixelLensScatteringModel(nnx.Module):
             x = layer(x)
             if i < len(self.layers) - 1:
                 x = nnx.leaky_relu(x)
-                # x = self.dropout(x)
 
-        trans_ref_amps = x[..., :2 * self.n_propagating_waves] + 1j * x[..., 2 * self.n_propagating_waves:]
-        power_per_sample = jnp.linalg.norm(trans_ref_amps, axis=-1)
-        normalized_trans_ref_amps = trans_ref_amps / power_per_sample[:, jnp.newaxis]
-        return normalized_trans_ref_amps
+        if self.include_transmission:
+            trans_ref_amps = x[..., :2 * self.n_propagating_waves] + 1j * x[..., 2 * self.n_propagating_waves:]
+            power_per_sample = jnp.linalg.norm(trans_ref_amps, axis=-1)
+            normalized_trans_ref_amps = trans_ref_amps / power_per_sample[:, jnp.newaxis]
+            return normalized_trans_ref_amps
+        return x[..., :self.n_propagating_waves] + 1j * x[..., self.n_propagating_waves:]
 
     def save(self, filename):
         _, state = nnx.split(self)
@@ -53,10 +59,16 @@ class SquarePixelLensScatteringModel(nnx.Module):
             filename: str,
             n_propagating_waves: int,
             n_lens_params: int,
-            hidden_layer_dims: list[int]
+            hidden_layer_dims: list[int],
+            include_transmission=False
     ):
         abstract_model = nnx.eval_shape(
-            lambda: SquarePixelLensScatteringModel(n_propagating_waves, n_lens_params, hidden_layer_dims, nnx.Rngs(0)))
+            lambda: SquarePixelLensScatteringModel(
+                n_propagating_waves,
+                n_lens_params,
+                hidden_layer_dims,
+                nnx.Rngs(0),
+                include_transmission))
         graph_def, abstract_state = nnx.split(abstract_model)
         with open(filename, 'rb') as f:
             state_restored = pickle.load(f)
@@ -66,27 +78,30 @@ class SquarePixelLensScatteringModel(nnx.Module):
 
 def train_forward_model(hidden_dims, batch_size, n_epochs, learning_rate):
     max_width = 300.
-    incidence_reference_amplitude = -1.20536745e-01 + 9.9270886e-01j  # -exp(1j * k0 * thickness)
-    data = jnp.load('ai_training_data/red_7x7_th500_p300_120k.npz')
+    # incidence_reference_amplitude = -1.20536745e-01 + 9.9270886e-01j  # -exp(1j * k0 * thickness)
+    # data = jnp.load('ai_training_data/red_7x7_th500_p300_120k.npz')
     # data = jnp.load('ai_training_data/red_8x8_th800_p300.npz')
+    data = jnp.load('ai_training_data/red_4x4_symmetries.npz')
 
     widths = jnp.array(data['widths'])
     widths = widths.reshape(widths.shape[0], -1)
     amplitudes = jnp.array(data['amps'])
-    amplitudes /= jnp.linalg.norm(amplitudes, axis=-1, keepdims=True)
+    # amplitudes /= jnp.linalg.norm(amplitudes, axis=-1, keepdims=True)
 
     n_propagating_waves = amplitudes.shape[-1] // 2
+    amplitudes = amplitudes[:, :n_propagating_waves]
     n_lens_params = widths.shape[-1]
 
     widths /= max_width
-    amplitudes = amplitudes.at[:, :n_propagating_waves].divide(incidence_reference_amplitude)
+    # amplitudes = amplitudes.at[:, :n_propagating_waves].divide(incidence_reference_amplitude)
 
     print('Widths shape:', widths.shape)
     print('Amps shape:', amplitudes.shape)
     print('n_propagating_waves:', n_propagating_waves)
     print('n_lens_params:', n_lens_params)
 
-    approximate_test_to_train_ratio = 1 / 6
+    # approximate_test_to_train_ratio = 1 / 6
+    approximate_test_to_train_ratio = 0.05
     total_n_samples = widths.shape[0]
     n_batches = int(((1 - approximate_test_to_train_ratio) * total_n_samples) // batch_size)
     n_training_samples = n_batches * batch_size
@@ -150,8 +165,8 @@ def train_forward_model(hidden_dims, batch_size, n_epochs, learning_rate):
 
 if __name__ == '__main__':
     train_forward_model(
-        hidden_dims=[1024, 1024, 1024],
+        hidden_dims=[32, 32, 32],
         batch_size=200,
-        n_epochs=500,
+        n_epochs=1000,
         learning_rate=1e-2
     )
--- ai_model_forward_evaluation.py
+++ ai_model_forward_evaluation.py
@@ -3,6 +3,8 @@ import jax.numpy as jnp
 
 from ai_model_forward import SquarePixelLensScatteringModel
 from scattering_solver_factory import prepare_lens_pixel_width_to_scattered_amplitudes_function
+from field_postprocessing import calculate_focusing_efficiency
+from fmmax import basis, fields, fmm
 
 import matplotlib.pyplot as plt
 
@@ -10,17 +12,29 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     jnp.set_printoptions(linewidth=10000)
 
-    wavelength=650
-    permittivity=4
-    n_pillars = 7
-    lens_subpixel_size=300
-    lens_thickness=500
+    # wavelength=650
+    # permittivity=4
+    # n_pillars = 4
+    # lens_subpixel_size=300
+    # lens_thickness=800
+    # model = SquarePixelLensScatteringModel.load(
+    #     filename='ai_models/red_4x4_forward',
+    #     n_propagating_waves=9,
+    #     n_lens_params=n_pillars ** 2,
+    #     hidden_layer_dims=[32, 32, 32]
+    # )
 
+    wavelength = 650
+    permittivity = 4
+    n_pillars = 7
+    lens_subpixel_size = 300
+    lens_thickness = 500
     model = SquarePixelLensScatteringModel.load(
         filename='ai_models/red_7x7_forward.pkl',
         n_propagating_waves=37,
         n_lens_params=n_pillars ** 2,
-        hidden_layer_dims=[1024] * 3
+        hidden_layer_dims=[1024] * 3,
+        include_transmission=True
     )
 
     true_widths_to_amps_f = prepare_lens_pixel_width_to_scattered_amplitudes_function(
@@ -35,29 +49,50 @@ if __name__ == "__main__":
 
     # widths = jnp.zeros((n_pillars, n_pillars))
     # widths = 200 * jnp.ones((n_pillars, n_pillars))
-    widths = 300 * jax.random.uniform(key=jax.random.key(0), shape=(n_pillars ** 2,))
+    # widths = 300 * jax.random.uniform(key=jax.random.key(0), shape=(n_pillars ** 2,))
+    widths = jnp.array([
+        [90,  90,  160, 200, 160, 90,  90],
+        [90,  85,  160, 195, 160, 85,  90],
+        [160, 160, 220, 235, 220, 160, 160],
+        [200, 195, 235, 260, 235, 195, 200],
+        [160, 160, 220, 235, 220, 160, 160],
+        [90,  85,  160, 195, 160, 85,  90],
+        [90,  90,  160, 200, 160, 90,  90]
+    ])
 
-    predicted_amps = jnp.squeeze(model(widths.reshape(1, -1)))
+    predicted_amps = jnp.squeeze(model(widths.reshape(1, -1) / lens_subpixel_size))
     true_amps = true_widths_to_amps_f(widths)
     true_amps = true_amps.at[:len(true_amps) // 2].divide(reference_a00)
+    # true_amps = true_amps[:len(true_amps) // 2] / reference_a00
     true_amps /= jnp.linalg.norm(true_amps)
+
+    propagating_basis_indices = basis.generate_expansion(
+        basis.LatticeVectors(basis.X, basis.Y), model.n_propagating_waves).basis_coefficients
+    total_lens_period = n_pillars * lens_subpixel_size
+    K = 2 * jnp.pi / total_lens_period
+    kx, ky = propagating_basis_indices.T * K
+    k0 = 2 * jnp.pi / wavelength
+    kz = jnp.sqrt(k0 ** 2 - kx ** 2 - ky ** 2)
+    focal_length = 4000
+    true_amps_propagated = true_amps[:model.n_propagating_waves] * jnp.exp(1j * kz * focal_length)
+    predicted_amps_propagated = predicted_amps[:model.n_propagating_waves] * jnp.exp(1j * kz * focal_length)
+    true_efficiency = calculate_focusing_efficiency(true_amps_propagated[:model.n_propagating_waves], propagating_basis_indices)
+    predicted_efficiency = calculate_focusing_efficiency(predicted_amps_propagated[:model.n_propagating_waves], propagating_basis_indices)
+    print(true_efficiency, predicted_efficiency)
 
     plt.plot(jnp.abs(predicted_amps), 'o', label='Predicted')
     plt.plot(jnp.abs(true_amps), '--', label='True')
+    plt.title('Predicted and True amplitudes for random 7x7 lens')
     plt.legend()
     plt.grid()
     plt.show()
 
-    # fig, ax = plt.subplots(1, 2)
-    # ax[0].plot(true_amps.real, '--', label='True')
-    # ax[0].plot(predicted_amps.real, 'o', label='Predicted')
-    # ax[0].legend()
-    # ax[0].set_title('Imag')
-    # ax[1].plot(true_amps.imag, '--', label='True')
-    # ax[1].plot(predicted_amps.imag, 'o', label='Predicted')
-    # ax[1].legend()
-    # ax[1].set_title('Imag')
-    # plt.show()
+    plt.plot(jnp.angle(predicted_amps), 'o', label='Predicted')
+    plt.plot(jnp.angle(true_amps), '--', label='True')
+    plt.title('Predicted and True phases for random 7x7 lens')
+    plt.legend()
+    plt.grid()
+    plt.show()
 
     print(predicted_amps)
     print(true_amps)
